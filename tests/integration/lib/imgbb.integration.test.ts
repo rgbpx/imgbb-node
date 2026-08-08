d0172337e9d6757ef732f69f0bef840076b2acab
rgbpx <rgbpx@proton.me>
2026-08-09 02:23:46 +0400

    test: add base64 upload integration tests

--- tests/integration/lib/imgbb.integration.test.ts
+++ tests/integration/lib/imgbb.integration.test.ts
@@ -1,7 +1,7 @@
 import { readFile } from "node:fs/promises";
 import path from "node:path";
 import { describe, expect, it } from "vitest";
-import { uploadFile } from "@src/lib/imgbb.js";
+import { uploadFile, uploadBase64 } from "@src/lib/imgbb.js";
 
 const key = process.env.IMGBB_API_KEY as string;
 
@@ -85,6 +85,57 @@ describe("ImgBB Integration", () => {
       expect(result.data.thumb.url).toContain(convertedFilename);
       expect(result.success).toBe(true);
       expect(result.status).toBe(200);
+    });
+  });
+
+  describe("uploadBase64", () => {
+    it.concurrent("should return result", async () => {
+      const expiration = 60;
+      const name = "rgbpx";
+      const extension = "png";
+      const filename = `${name}.${extension}`;
+      const mime = `image/${extension}`;
+      const width = 4;
+      const height = 4;
+      const size = 90;
+      const base64 =
+        "iVBORw0KGgoAAAANSUhEUgAAAAQAAAAEAgMAAADUn3btAAAAAXNSR0IB2cksfwAAAAlwSFlzAAALEwAACxMBAJqcGAAAAAlQTFRF/wAAAP8AAAD/LUrNigAAAAxJREFUeJxjFGMEQwABfABdo2H+CQAAAABJRU5ErkJggg==";
+
+      const result = await uploadBase64(base64, { key, name, expiration });
+
+      expect(result.data.title).toContain(name);
+      expect(result.data.url).toContain(filename);
+      expect(result.data.display_url).toContain(filename);
+      expect(result.data.width).toBe(width);
+      expect(result.data.height).toBe(height);
+      expect(result.data.size).toBe(size);
+      expect(result.data.expiration).toBe(expiration);
+      expect(result.data.image.filename).toBe(filename);
+      expect(result.data.image.name).toBe(name);
+      expect(result.data.image.mime).toBe(mime);
+      expect(result.data.image.extension).toBe(extension);
+      expect(result.data.image.url).toContain(filename);
+      expect(result.data.image.filename).toBe(filename);
+      expect(result.data.thumb.name).toBe(name);
+      expect(result.data.thumb.mime).toBe(mime);
+      expect(result.data.thumb.extension).toBe(extension);
+      expect(result.data.thumb.url).toContain(filename);
+      expect(result.success).toBe(true);
+      expect(result.status).toBe(200);
+    });
+
+    it.concurrent("should throw invalid base64 error", async () => {
+      const base64 = "bad base64";
+      const mockStatusCode = 400;
+      const mockStatusText = "Bad Request";
+      const mockErrorCode = 120;
+      const mockErrorMessage = "Invalid base64 string.";
+
+      const resultPromise = uploadBase64(base64, { key });
+
+      await expect(resultPromise).rejects.toThrow(
+        `HTTP ${mockStatusCode} ${mockStatusText} ImgBB ${mockErrorCode} ${mockErrorMessage}`
+      );
     });
   });
 });
